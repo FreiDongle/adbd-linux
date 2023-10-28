a7f22e44362e8dcecbe8b18f3b0d58db1af72c90
ttimasdf <opensource@rabit.pw>
2023-10-28 19:50:27 +0800

    Add CROSS_PREFIX and new variable for pkg-config

--- include.mk
+++ include.mk
@@ -10,3 +10,9 @@ mandir ?= $(datadir)/man
 sysconfdir ?= $(prefix)/etc
 OPT_CFLAGS ?= -O2 -g
 OPT_CXXFLAGS ?= -O2 -g
+
+RANLIB = ${CROSS_PREFIX}ranlib
+AR = ${CROSS_PREFIX}ar
+CXX = ${CROSS_PREFIX}g++
+CC = ${CROSS_PREFIX}gcc
+PKG_CONFIG = ${CROSS_PREFIX}pkg-config
